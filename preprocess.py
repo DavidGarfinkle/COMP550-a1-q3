6e04bfe7661154e6ec011488f143587bcd63d505
DavidGarfinkle <david.garfinkle@mail.mcgill.ca>
2018-09-25 14:51:09 -0400

    Preprocesses and stores bags of words in mongodb

--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,25 @@
 ##
 # preprocess.py
 #
-# Script for reading the text data and extracting features
-# Stores the result as a pickled pandas dataframe
+# Script for reading the text data and storing phrases as feature vectors bags-of-words style
+# Stores the bags of words
 
 import pandas as pd
+import numpy as np
 import nltk
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+
+# Connect to mongo atlas
+import pymongo
+from credentials import mongo_password
+
+#Mongo Atlas
+#client = pymongo.MongoClient("mongodb+srv://david156:{}@cluster0-ixj0v.mongodb.net/test?retryWrites=true".format(mongo_password))
+client = pymongo.MongoClient("localhost", 8000)
+db = client.sentiment_analysis
+db_collection = db.bags_of_words
 
 NEG_PATH = "data/rt-polarity.neg"
 POS_PATH = "data/rt-polarity.pos"
@@ -72,6 +83,7 @@ from itertools import product
 from sklearn.feature_extraction.text import CountVectorizer
 
 #for ngram_range, data, min_df in product(*list(data_parameters.values())):
+i = 0
 for ngram_range, (morph, data), min_df in product(
         ((1, 1), (1,2)),
         (
@@ -84,8 +96,17 @@ for ngram_range, (morph, data), min_df in product(
 
     cv = CountVectorizer(min_df=min_df, ngram_range=ngram_range)
 
-    X = cv.fit_transform(data['phrase'].values).toarray()
+    X = cv.fit_transform(data['phrase'].values)
     y = raw_data['class'].values
 
-    print(ngram_range, morph, min_df)
-    print(X)
+    print("Inserting bag of words {} {} {}".format(ngram_range, morph, min_df))
+    i += 1
+    docId = db_collection.insert_one({
+        'ngram_range': ngram_range,
+        'morph': morph,
+        'min_df': min_df
+    }).inserted_id
+    with open('data/db/{}.npy'.format(docId), 'wb') as f:
+        np.save(f, X)
+
+print("inserted {} bags".format(i))
